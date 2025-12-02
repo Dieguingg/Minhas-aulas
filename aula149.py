b8681eb933bc321d3db2129ed8777f4bacaa3c2a
Dieguingg <diegofeltrin590@gmail.com>
2025-12-02 10:06:42 -0300

    Add files via upload

---
+++ aula149.py
@@ -0,0 +1,7 @@
+import aula149_uma_linha
+
+# print(dir(aula149_uma_linha))
+# print(aula149_uma_linha.__doc__)
+# print(aula149_uma_linha.__file__)
+# print(aula149_uma_linha.__name__)
+help(aula149_uma_linha)
